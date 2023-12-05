fe2cea23a66b325d4e9144f152aaa2627c2dc131
marco <marco.nunes.senac@gmail.com>
2023-12-05 12:02:38 -0300

    051223.7

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('https://www7.pe.senac.br/certificadocongresso/usuarios', async (req, res) => {
+app.get('/certificadocongresso/usuarios', async (req, res) => {
   const nome = req.query.nome;
   console.log('Nome recebido no servidor: ' + nome);
   
